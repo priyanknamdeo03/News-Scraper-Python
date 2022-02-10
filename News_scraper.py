d900f601f977f6183277e75af60e607852a7975b
priyanknamdeo <priyanknamdeo03@gmail.com>
2022-02-10 11:14:11 +0530

    formatted according to PEP8

--- News_scraper.py
+++ News_scraper.py
@@ -1,80 +1,76 @@
 import requests
 import bs4
 import json
-import time
 
+URL = 'https://www.ndtv.com/top-stories'
+FILE_NAME = 'news.html'  
 
 class News:
     # initializing the class members
-    def __init__(self, url, filename):
+    def __init__(self, url: str, filename: str) -> None:
+        self.list_of_news_articles = [] 
         self.url = url
         self.filename = filename
-        self.response = requests.get(self.url)  # Object
-        self.soup = bs4.BeautifulSoup(self.response.text, "html.parser")
-        self.list_of_news_articles = []
+        self.response = requests.get(self.url)
+        self.soup = bs4.BeautifulSoup(self.response.text, "html.parser") 
 
     # ----------------------------------------------------------------------
 
     # Saving the parsed Website code in Local File
-    def save_code(self):
+    def save_code(self) -> None:
         formatted_text = self.soup.prettify()
-        with open(self.filename, "w", encoding="utf-8") as f:
-            f.write(formatted_text)
+
+        with open(self.filename, "w", encoding="utf-8") as file:
+            file.write(formatted_text)
 
     # ----------------------------------------------------------------------
 
-    #   Extracting The webpage Code ,Parsing and saving the Http request data in a Local file
-    def news_article_scraping(self):
-
+    #  Extracting The webpage Code ,Parsing and saving the HTTP request data in a Local file
+    def news_article_scraping(self) -> None:
         news_article = self.soup.find('div', class_='lisingNews')
+        
         for i in news_article.find_all('div', class_='news_Itm adBg'):
             i.decompose()
 
-        n = news_article.find_all('div', class_='news_Itm')
+        news_tags = news_article.find_all('div', class_='news_Itm')
         count = 0
-        for news in n:
+
+        for news in news_tags:
             article_number = count
             article_name = news.find('h2', class_='newsHdng').text
             news_agency_name = news.find('span', class_='posted-by').text.replace(' ', '').split("|")[0]
             news_published_date = news.find('span', class_='posted-by').text.split("|")[0].split(",")[0]
-            # news_location = news.find('span', class_='posted-by').text.split("|")[1].split(",")
-            # print(news_location)
+            
             news_info = news.find('p', class_='newsCont').text
             news_link = news.find('h2', class_='newsHdng').find('a')['href']
             count += 1
-            # print(count)
-            data = {'News Post Number': article_number,
-                    'Article_Name': article_name,
-                    'News Publishing Agency Name': news_agency_name,
-                    'News Published Date': news_published_date,
-                    # 'News Published Location': news_location,
-                    'News Info': news_info,
-                    'News Link': news_link
-                    }
+            
+            data = {  
+                'News Post Number': article_number,
+                'Article_Name': article_name,
+                'News Publishing Agency Name': news_agency_name,
+                'News Published Date': news_published_date,
+                'News Info': news_info,
+                'News Link': news_link
+                }
             self.list_of_news_articles.append(data)
-        # print(self.list_of_news_articles)
-        with open(f'News Articles.json', 'w') as f:
-            json.dump(self.list_of_news_articles, f, indent=4)
+
+        with open(f'News Articles.json', 'w') as file:  
+            json.dump(self.list_of_news_articles, file, indent=4)
         print(f'{count} New Articles Saved In : News Articles.json')
 
 
-def main():
-    url = 'https://www.ndtv.com/top-stories'
-    file_name = 'news.html'  # input("Enter Filename with Extension To Store The Parsed News-site : ")
-    # while True:
-    n = News(url, file_name)
-    n.save_code()
-    n.news_article_scraping()
-    # print("Waiting For 30 Seconds........")
-    # time.sleep(30)
+def main() -> None:
+    daily_news = News(URL, FILE_NAME)
+    daily_news.save_code()
+    daily_news.news_article_scraping()
 
 
 if __name__ == "__main__":
     try:
         main()
-    except KeyboardInterrupt:
-        exit()
-    # main()
+    except Exception as e: 
+        print("Exception", e.message)
 
 
 
